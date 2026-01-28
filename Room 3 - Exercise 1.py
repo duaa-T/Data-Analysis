ca83740baf7812a7a0612873dbfd6fdf28b19d59
duaa-T <duaa.abdelbagi93@gmail.com>
2026-01-28 15:47:13 +0400

    Update Room 3 - Exercise 1.py

--- Room 3 - Exercise 1.py
+++ Room 3 - Exercise 1.py
@@ -143,4 +143,20 @@ def sum(a,b):
    print("Sum is ",a+b)
 sum(50,67)
 
+# Authors:
+# 1. Adeyonu Temidayo Adebunmi WTF/2025/8395
+# 2. OJO ESTHER AYOMIDE - WTF/2025/5212
+# 3. Duaa Abdelbagi WTF/2025/7390
+# 4. Mercy Sunday - WTF/2025/7056
+# 5. Kanene Lillian Winnie WTF/2025/9552
+# 6. Sanelisiwe Pamela - WTF/2025/4987
+# 7. Yvonne Akoth - WTF/2025/6527
+# 8. Priscilla Salako- WTF/2025/6828
+# 9. Stephanie Ng'ang'a- WTF/2025/8560
+# 10. Sophie Wabuge - WTF/2025/8623
+# 11. Ogundiran Tomilola WTF/2025/6105
+# 12. Nwokedi celestina -WTF/2025/6470
+# 13.Janet Wangari Mwangi-WTF/2025/4125
+# 14.Priscilla Aremu-WTF/2025/9090
+
 
