1ae88da2b96e1f428934b15355b499854c248c8e
duaa-T <duaa.abdelbagi93@gmail.com>
2026-01-28 15:08:40 +0400

    Add files via upload

---
+++ Room 3 - Exercise 1.py
@@ -0,0 +1,145 @@
+#Q1
+age = 32
+print(age)
+
+#Q2
+a = 10
+b = 5
+print(a+b)
+
+#Q3
+a=2
+b=34
+print(a*b)
+
+#Q4
+a = 20
+b = 4
+print(a/b)
+
+#Q5
+print("Python is fun")
+
+#Q6
+Name = input("Enter Your Name ")
+print("Hello", Name)
+
+#Q7
+c=int(input("Give me a number"))
+d=int(input("Give me another number"))
+print(c+d)
+
+#Q8
+age = int(input("Enter Your Age: "))
+print("Next year you'll be ",age+1," years old")
+
+#Q9
+number=5
+print(number>10)
+
+#Q10
+number = 10
+if number % 2 == 0:
+   print("Even")
+else:
+   print("Odd")
+
+#Q11
+a = 15
+b = 15
+print(a==b)
+
+#Q12
+a = 100
+print(1<=a<=100)
+
+#Q13
+score = 60
+if score >= 50: 
+   print("pass")
+else: 
+   print("fail")
+
+#Q14
+Age = int(input("Enter your age: "))
+if Age>=18 :
+   print("Adult")
+else:
+   print("Minor")
+
+#Q15
+number = 0
+if number<0 :
+   print("Negative")
+elif number==0: 
+   print("Zero is Neutral")
+else :
+   print("Positive")
+
+#Q16
+password=input("Enter Password: ")
+if password=="Python123":
+   print("Correct")
+else : 
+   print("Incorrect")
+
+#Q17
+for i in range(1,11) :
+   print(i) 
+
+#Q18
+for a in range(5):
+   print("Duaa")
+
+#Q19
+i=1
+while i<=5 :
+   print(i)
+   i+=1 
+
+#Q20
+for i in range(2,21,2) :
+   print(i)
+
+#Q21
+fruits=["banana","orange","cherry","grape","berries"]
+print(fruits)
+
+#Q22
+fruits=["banana","orange","cherry","grape","berries"]
+print(fruits[0])
+print(fruits[-1])
+
+#Q23
+fruits=["banana","orange","cherry","grape","berries"]
+fruits.append("apple")
+print(fruits)
+
+#Q24
+fruits=["banana","orange","cherry","grape","berries","apple"]
+for i in fruits:
+   print(i)
+
+#Q25
+colors=("Red","Blue","Yellow")
+print(colors)
+
+#Q26
+numbers={1,2,2,3,3,4,4}
+print(i) 
+
+#Q27 & Q28
+Student={"name":"Duaa","Age":"32","Course":"Data Science and Engineering "}
+print(Student)
+print(Student["name"])
+
+#Q29
+def welcome():
+   print("Welcome to Python")
+welcome() 
+
+#Q30
+def sum(a,b):
+   print("Sum is ",a+b)
+sum(50,67)
+
